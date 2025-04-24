c66fb22a5b18a717842b8409adda72e4f787c427
Bunyodjon Ermatov <ermatovbunyod37@gmail.com>
2025-04-24 20:46:11 +0500

    roll dice

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,14 @@ import 'package:first_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          Color.fromARGB(255, 33, 5, 109),
+          Color.fromARGB(255, 68, 21, 149),
+        ),
+      ),
+    ),
+  );
 }
